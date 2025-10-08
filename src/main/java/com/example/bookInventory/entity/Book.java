476b3859b5ef65e28f4f14110f34d7f93a2dd31b
Gangadharan <gangadharan.r-p@capgemini.com>
2025-10-08 22:05:38 +0530

    second commit - author and book

--- src/main/java/com/example/bookInventory/entity/Book.java
+++ src/main/java/com/example/bookInventory/entity/Book.java
@@ -26,4 +26,74 @@ public class Book {
 	
 	@Column(name = "publisherId")
 	private Integer publisherId;
+
+	public Book() {
+		super();
+	}
+
+	public Book(String isbn, String title, String description, Integer category, String edition, Integer publisherId) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.description = description;
+		this.category = category;
+		this.edition = edition;
+		this.publisherId = publisherId;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getCategory() {
+		return category;
+	}
+
+	public void setCategory(Integer category) {
+		this.category = category;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	public Integer getPublisherId() {
+		return publisherId;
+	}
+
+	public void setPublisherId(Integer publisherId) {
+		this.publisherId = publisherId;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [isbn=" + isbn + ", title=" + title + ", description=" + description + ", category=" + category
+				+ ", edition=" + edition + ", publisherId=" + publisherId + "]";
+	}
+	
+	
 }
--- src/main/java/com/example/bookInventory/service/BookService.java
+++ src/main/java/com/example/bookInventory/service/BookService.java
@@ -1,5 +1,18 @@
 package com.example.bookInventory.service;
 
-public interface BookService {
+import java.util.List;
 
+import com.example.bookInventory.entity.Book;
+
+public interface BookService {
+	Book addBook(Book book);
+	List<Book> getAllBooks();
+	Book getBookByIsbn(String isbn);
+	List<Book> getBooksByTitle(String title);
+	List<Book> getBooksByCategory(Integer category);
+	List<Book> getBooksByPublisherId(Integer publisherId);
+	Book updateBookTitle(String isbn, String newTitle);
+	Book updateBookDescription(String isbn, String newDesc);
+	Book updateBookCategory(String isbn, Integer newCategory);
+	Book updateBookPublisher(String isbn, Integer newPublisher);
 }
